64c557d46153ab644ab716b84a3d336e8df230e9
Corrine Sing <corrine.sing@gmail.com>
2023-03-22 15:28:40 +0000

    2nd commit

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,2 +1,48 @@
 def return_10():
     return 10
+
+def add(num_1, num_2):
+    return(num_1 + num_2)
+
+def subtract(num_1, num_2):
+    return(num_1 - num_2)
+    
+def multiply (num_1, num_2):
+    return(num_1 * num_2)
+
+def divide(num_1, num_2):
+    return(num_1 / num_2)
+
+def length_of_string(string):
+    return(len(string))
+
+def join_string(string_1, string_2):
+    return(string_1 + string_2)
+
+def add_string_as_number(num_1, num_2):
+    return(int(num_1) + int(num_2))
+
+
+def number_to_full_month_name(num):
+    if num == 1:
+        return("January")
+    if num == 3:
+        return("March")
+    if num == 9:
+        return("September")
+
+def number_to_short_month_name(num):
+    if num == 1:
+        return("Jan")
+    if num == 4:
+        return("Apr")
+    if num == 10:
+        return("Oct")
+
+
+
+
+
+
+    
+
